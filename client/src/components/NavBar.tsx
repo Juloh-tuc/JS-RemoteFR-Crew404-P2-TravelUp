5afdec734e533058c8207fce5c089fff7bf0c34f
Juloh-tuc <julie.tuco@gmail.com>
2024-11-10 12:14:33 +0100

    amélioration Nvabar.tsx

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,23 +4,33 @@ import "../components/NavBar.css";
 function NavBar() {
   return (
     <header className="navbar">
-      <img
+		 <img
         src="../public/img/logo.png"
         className="logo"
         alt="une terre avec un pins rouge au couleur de notre charte"
-      />
+        />
+  );
       <nav className="nav">
-        <a href="#about" className="nav-link">
-          About
-        </a>
-        <a href="#apropos" className="nav-link">
-          A propos
-        </a>
-        <a href="#contact" className="nav-link">
-          Contact
-        </a>
+        <ul>
+          <li>
+            <a href="#about" className="nav-link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#apropos" className="nav-link">
+              A propos
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
+	
 }
 export default NavBar;
