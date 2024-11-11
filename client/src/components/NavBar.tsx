d64cc2e4238a23653988eb6522b733d20f46f86c
Juloh-tuc <julie.tuco@gmail.com>
2024-11-11 09:36:22 +0100

    navBar qui se déploie à la souris

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,35 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
 import "../components/NavBar.css";
 
 function NavBar() {
+	 const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <header className="navbar">
-		 <img
-        src="../public/img/logo.png"
-        className="logo"
-        alt="une terre avec un pins rouge au couleur de notre charte"
+    <nav
+      className={`navbar ${isExpanded ? 'expanded' : ''}`}
+      onMouseEnter={() => setIsExpanded(true)}
+      onMouseLeave={() => setIsExpanded(false)}
+    >
+      <div className="navbar-logo">
+        <img
+          src="../public/img/logo.png"
+          className="logo"
+          alt="une terre avec un pins rouge au couleur de notre charte"
         />
-  );
-      <nav className="nav">
-        <ul>
-          <li>
-            <a href="#about" className="nav-link">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#apropos" className="nav-link">
-              A propos
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="nav-link">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      </div>
+      <ul className="navbar-links">
+        <a href="#about">
+          About
+        </a>
+        <a href="#home">
+          Home
+        </a>
+        <a href="#contact">
+          Contact
+        </a>
+      </ul>
+    </nav>
   );
 	
 }
