5237a4c6366e60cd7813b1b8519656248e202931
Juloh-tuc <julie.tuco@gmail.com>
2024-11-12 11:58:07 +0100

    responsiv en cours

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../components/NavBar.css";
 
 function NavBar() {
-	 const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <nav
-      className={`navbar ${isExpanded ? 'expanded' : ''}`}
-      onMouseEnter={() => setIsExpanded(true)}
-      onMouseLeave={() => setIsExpanded(false)}
-    >
-      <div className="navbar-logo">
-        <img
-          src="../public/img/logo.png"
-          className="logo"
-          alt="une terre avec un pins rouge au couleur de notre charte"
-        />
-      </div>
-      <ul className="navbar-links">
-        <a href="#about">
-          About
-        </a>
-        <a href="#home">
-          Home
-        </a>
-        <a href="#contact">
-          Contact
-        </a>
-      </ul>
-    </nav>
+    <>
+      <img
+        className="logo"
+        src="/img/logo.png"
+        alt="une terre avec un pins rouge au couleur de notre charte"
+      />
+
+      <nav
+        className={`navbar ${isExpanded ? "expanded" : ""}`}
+        onMouseEnter={() => setIsExpanded(true)}
+        onMouseLeave={() => setIsExpanded(false)}
+      >
+        <ul className="navbar-links">
+          <li>
+            <a href="#about">
+              <img src="/img/about.png" alt="About" />
+			  
+            </a>
+          </li>
+          <li>
+            <a href="#home">
+              <img src="/img/home.png" alt="Home" />
+			  
+            </a>
+          </li>
+          <li>
+            <a href="#contact">
+              <img src="/img/contact.png" alt="Contact" />
+			  
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
-	
 }
+
 export default NavBar;
+
