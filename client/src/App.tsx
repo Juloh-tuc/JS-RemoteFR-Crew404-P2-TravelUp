2e7894561e69265a4e27c710bc0ca1253970cb27
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-11-18 18:30:01 +0100

    fix biome 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import QuestionsForm from "./components/QuestionsForm";
 import "./App.css";
 
 function App() {
