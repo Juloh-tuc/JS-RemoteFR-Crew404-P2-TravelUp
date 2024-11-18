cdd5fc404a778952d6c906f303cde854f7a5a674
Juloh-tuc <julie.tuco@gmail.com>
2024-11-18 15:13:20 +0100

    contact

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,15 @@
-import Checkbox from "./components/Checkbox";
-import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import QuestionsForm from "./components/QuestionsForm";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Questionnaire</h1>
-      <QuestionsForm />
-
-      <div>
-        <NavBar />
-        <Checkbox />
-        <Footer />
-      </div>
-    </div>
+    <>
+      <NavBar />
+      <main style={{ paddingTop: "2rem" }}>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
