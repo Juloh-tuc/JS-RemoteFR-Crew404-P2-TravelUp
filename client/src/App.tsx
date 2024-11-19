36382039a496c3ff8f12b0658eba82d4ada823d3
Camille L <b.lemortcamille@gmail.com>
2024-11-19 15:31:06 +0100

    Nouvelles modifs, suppression Map.css et Map.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,13 @@
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-import Map from "./components/Map";
 import QuestionsForm from "./components/QuestionsForm";
 import "./App.css";
 
 function App() {
   return (
-
     <div className="App">
       <QuestionsForm />
       <div>
-        <div className="Worldmap">
-          <Map />
-        </div>
         <NavBar />
         <Footer />
       </div>
