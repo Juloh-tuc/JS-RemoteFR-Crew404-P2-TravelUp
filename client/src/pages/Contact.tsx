1f977ad13dcb57e8f7814f77821aa2e45736b12f
Charles Catto <charlescatto@outlook.fr>
2024-11-20 11:19:05 +0100

    Adding some changes/fashion to the NavBar and fixing Footer

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Contact.css";
 import "../main.tsx";
 
 function Contact() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [backgroundImage, setBackgroundImage] = useState<string>("");
+
+  useEffect(() => {
+    const images = [
+      "https://images.pexels.com/photos/257092/pexels-photo-257092.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/1108701/pexels-photo-1108701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/2166553/pexels-photo-2166553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/29032545/pexels-photo-29032545/free-photo-of-spectacular-cliffs-of-etretat-in-normandy.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    ];
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    setBackgroundImage(randomImage);
+  }, []);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -21,7 +34,15 @@ function Contact() {
   };
 
   return (
-    <body className="contact-background customizable">
+    <body
+      className="contact-background customizable"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+      }}
+    >
       <div className="contact-form-container">
         <h2>Contactez-nous</h2>
         {isSubmitted ? (
